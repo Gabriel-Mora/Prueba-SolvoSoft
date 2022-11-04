c452c6098c5e83612af3495b96885adb3394e493
Gabriel-Mora <71365238+Gabriel-Mora@users.noreply.github.com>
2022-11-04 13:46:15 -0600

    commit v2

--- practica.py
+++ practica.py
@@ -9,22 +9,44 @@ baraja=["2C","2D","2T","2P","3C","3D2",
         "9T","9P","JC","JD","JT","JP","QC","QD","QT","QP"
         "KC","KD","KT", "KP","AC","AD","AT","AP"]
 
-
-
-
+def obtenerUnaCarta(baraja):
+    carta= random.choice(baraja)
+    return carta
+    
 def ObtenerCartaJugador(baraja):
+    
     i=0
     barajaJugador=[]
     for i in range(2):
         carta= random.choice(baraja)
         barajaJugador.append(carta)
+        baraja.pop(carta)
     return barajaJugador
 
-def Menu(cant_jugadores):
-    for i in range(cantJugadores):
+def ObtenerCartaJugadores(cant_jugadores, baraja):
+    lista_barajas=[]
+    for i in range(cant_jugadores):
+        
+        lista_barajas.append(ObtenerCartaJugador(baraja))
+    return lista_barajas
+
+def Menu(cant_jugadores, baraja ):
+    
+    for i in range (cant_jugadores):
+        respuesta= int(input("Desea otra Carta? 1. Sí o 2.No: "))
+
+        if respuesta== 1:
+            
+
+        else if respuesta ==2:
+            
+        
+        
 
     
 print(ObtenerCartaJugador(baraja))
+print(Menu(cant_jugadores, baraja))
+
         
         
 
